45d439d6ad47031cd128f4c6bb458ac54ec74443
TheHuntsman4 <anikethvij464@gmail.com>
2023-03-14 18:22:42 +0530

    first firebase project push

---
+++ firebase/flutter_ci_script_beta.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+DIR="${BASH_SOURCE%/*}"
+source "$DIR/flutter_ci_script_shared.sh"
+
+declare -a CODELABS=(
+  "adaptive_app"
+  "animated-responsive-layout"
+  "boring_to_beautiful"
+  "cookbook"
+  # TODO(DomesticMouse): Use 'const' with the constructor to improve performance.
+  # "cupertino_store"
+  "dart3"
+  # TODO(DomesticMouse): Use 'const' with the constructor to improve performance.
+  # "dartpad_codelabs"
+  "deeplink_cookbook"
+  "ffigen_codelab"
+  # TODO(DomesticMouse): version solving failed.
+  # "firebase-auth-flutterfire-ui"
+  "firebase-emulator-suite"
+  # TODO(DomesticMouse): version solving failed.
+  # "firebase-get-to-know-flutter"
+  "flame-building-doodle-dash"
+  "github-client"
+  "google-maps-in-flutter"
+  "haiku_generator"
+  # TODO(DomesticMouse): Use 'const' with the constructor to improve performance.
+  # "in_app_purchases"
+  "namer"
+  "plugin_codelab"
+  "star_counter"
+  "testing_codelab"
+  "tfagents-flutter"
+  "tfrs-flutter"
+  "tfserving-flutter"
+  "tooling"
+  "webview_flutter"
+  )
+
+# Plugin codelab is failing on ubuntu-latest in CI.
+if [[ "$OSTYPE" != "linux-gnu"* ]]; then
+  CODELABS+=("plugin_codelab")
+fi
+
+ci_codelabs "beta" "${CODELABS[@]}"
+
+echo "== END OF TESTS"
---
+++ firebase/flutter_ci_script_stable.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+DIR="${BASH_SOURCE%/*}"
+source "$DIR/flutter_ci_script_shared.sh"
+
+declare -a CODELABS=(
+  "adaptive_app"
+  "animated-responsive-layout"
+  "boring_to_beautiful"
+  "cookbook"
+  "cupertino_store"
+  "dartpad_codelabs"
+  "deeplink_cookbook"
+  "ffigen_codelab"
+  "firebase-auth-flutterfire-ui"
+  "firebase-emulator-suite"
+  "firebase-get-to-know-flutter"
+  "flame-building-doodle-dash"
+  "github-client"
+  "google-maps-in-flutter"
+  "haiku_generator"
+  "in_app_purchases"
+  "namer"
+  "plugin_codelab"
+  "star_counter"
+  "testing_codelab"
+  "tfagents-flutter"
+  "tfrs-flutter"
+  "tfserving-flutter"
+  "tooling"
+  "webview_flutter"
+  )
+
+# Plugin codelab is failing on ubuntu-latest in CI.
+if [[ "$OSTYPE" != "linux-gnu"* ]]; then
+  CODELABS+=("plugin_codelab")
+fi
+
+ci_codelabs "stable" "${CODELABS[@]}"
+
+echo "== END OF TESTS"
